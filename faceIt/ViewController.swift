bf9d9c2b8687853cd721e5ee6bd5fd413ceb217d
Jingwei Zhang <jwdaniel.zhang@mail.utoronto.ca>
2018-07-11 11:56:07 +0800

    Initial Commit

---
+++ faceIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  faceIt
+//
+//  Created by 张经纬 on 2018/7/11.
+//  Copyright © 2018 Jingwei Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
